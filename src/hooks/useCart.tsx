8cad92dcaa1228c721ff64556b9fe92872326f6d
Matheus Santos Soares <mss.soares22@gmail.com>
2021-09-21 00:05:18 -0300

    chanlenge completed

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -18,33 +18,75 @@ interface CartContextData {
   removeProduct: (productId: number) => void;
   updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
+interface CardAmountResponse {
+  id: number;
+  amount: number;
+}
 
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCard = [...cart];
+      const productExists = updatedCard.find((item) => item.id === productId);
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`/products/${productId}`);
+
+        if (product.status === 404) {
+          throw new Error();
+        }
+
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        };
+        updatedCard.push(newProduct);
+      }
+      setCart(updatedCard);
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCard));
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const updatedCard = [...cart];
+      const productIndex = updatedCard.findIndex(
+        (product) => product.id === productId
+      );
+      if (productIndex >= 0) {
+        updatedCard.splice(productIndex, 1);
+        setCart(updatedCard);
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCard));
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error("Erro na remoção do produto");
     }
   };
 
@@ -53,9 +95,33 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+
+      if (amount > stockAmount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      const updatedCard = [...cart];
+      const productExists = updatedCard.find(
+        (product) => product.id === productId
+      );
+
+      if (productExists) {
+        productExists.amount = amount;
+        setCart(updatedCard);
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCard));
+      } else {
+        toast.error("Erro na alteração de quantidade do produto");
+        throw new Error();
+      }
     } catch {
-      // TODO
+      toast.error("Erro na alteração de quantidade do produto");
     }
   };
 
